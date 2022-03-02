6cb8f71533a37254a8e8e108cec8317b3ff841e4
gaelle kerforne <stage.seeweb@gmail.com>
2022-03-02 15:23:42 +0100

    derniere phase mise en prod

--- inc/init.inc.php
+++ inc/init.inc.php
@@ -3,15 +3,20 @@
 session_start();
 //PREMIRE LIGNE DE CODE, se positionne en haut et en premier avant tout traitements php
 //------------------------------------------------------------
-//Connexion à la BDD : 'boutique'
-$pdo  = new PDO('mysql:host=localhost;dbname=gk_wf3_php_intermediaire_gaelle', 'root', 'root' , array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_WARNING ) );
+//Connexion à la BDD :
+//if()
+//$pdo  = new PDO('mysql:host=localhost;dbname=gk_wf3_php_intermediaire_gaelle', 'root', 'root' , array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_WARNING ) );
 
 //------------------------------------------------------------
 //definition d'une constante :
 if(is_dir('C:')){
+    $pdo  = new PDO('mysql:host=localhost;dbname=gk_wf3_php_intermediaire_gaelle', 'root', 'root' , array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_WARNING ) );
     $serverName = 'evalphp.test';
     define( 'URL', "http://".$serverName."/");
 }else{
+    $user = '';
+    $pass = '';
+    $pdo  = new PDO('mysql:host=localhost;dbname=gk_wf3_php_intermediaire_gaelle', $user, $pass , array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_WARNING ) );
     $serverName = 'evalphp.g.maxslid.com';
     define( 'URL', "https://".$serverName."/");
 }
@@ -29,5 +34,5 @@ $error = ''; //variable prévue pour recevoir les messages d'erreurs
 //Inclusion des fonctions :
 require_once "functions.inc.php";
 
-debug($_SERVER);
+//debug($_SERVER);
 //[HTTP_REFERER] => http://php_wf3.test/EvalPHP/index.php
